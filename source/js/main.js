2732fac22ec48638f6e8d085701d8270901f0851
Alexey <finnlega@gmail.com>
2023-07-19 16:09:08 +0800

    добавил изменение шкалы прогрес бара в отдельную функцию, изменил setTimeOut на setInterval

--- source/js/main.js
+++ source/js/main.js
@@ -1,21 +1,29 @@
-const progressIndex = document.querySelector('.progress-bar__index');
-const progressActive = document.querySelector('.progress-bar__active');
+/* Обновляем процент шкалы Progress bar */
+
+const updateProgress = (value) => {
+
+  const progressIndex = document.querySelector('.progress-bar__index');
+  const progressActive = document.querySelector('.progress-bar__active');
+
+  progressActive.style.width = value +'%';
+  progressIndex.textContent = value + '%';
+}
+
+/* начальное значение ширины полосы */
+
+let progressValue = 20;
 
 /* принимаем ширину полосы за 100% */
 
 const SCALE = 100;
 
-const getScale = () => {
+/* Каждые 100 млс увеличиваем значение */
 
-    progressActive.style.width = '20%';
-    const index = progressIndex.innerHTML.replace('%', '');
+const getScale = setInterval(() => {
+  progressValue++;
+  updateProgress(progressValue);
 
-    let newIndex = Number(index);
-    for (let i = newIndex; i < SCALE; i++) {
-      newIndex += 1;
-      progressActive.style.width = `${newIndex}%`;
-      progressIndex.textContent =  `${newIndex}%`;
-    }
-}
-
-setTimeout(getScale, 500);
+  if (progressValue === SCALE) {
+    clearInterval(getScale);
+  }
+}, 75);
